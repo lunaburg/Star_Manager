d6bf86d007116d3d80dfd7b76b3e61f88e0fd089
yukilat <yukilatfzc@gmail.com>
2026-09-15 18:59:43 +0800

    Stop tracking local source and temp folders

--- tmp/list_animation_names.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import sys
-sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "apps" / "backend" / ".vendor"))
-import UnityPy
-for path in map(Path, sys.argv[1:]):
-    env = UnityPy.load(str(path))
-    print("===", path)
-    for index, obj in enumerate(x for x in env.objects if x.type.name == "AnimationClip"):
-        print(index, obj.path_id, obj.read().m_Name)
--- 插件/GameBridge/wwwroot/js/config.js
+++
@@ -1,3 +0,0 @@
-const GAMEBRIDGE_BASE_URL = "http://127.0.0.1:7879";
-const NOTIFICATION_SOUND_URL = "assets/scene-ready.wav";
-const CHARACTER_EDITOR_BASE_URL = "http://127.0.0.1:5001";
